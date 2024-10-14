2ea8bf2694b22708aae90ea8a1e17b6695c02a1d
Sergio Barreto <ba.sergio@javeriana.edu.co>
2024-10-13 23:28:36 -0500

    RestController in UsusarioController

--- src/main/java/com/example/ArriendaTuFinca/controllers/UsuarioController.java
+++ src/main/java/com/example/ArriendaTuFinca/controllers/UsuarioController.java
@@ -7,21 +7,13 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.ArriendaTuFinca.DTOs.UsuarioDTO;
 import com.example.ArriendaTuFinca.services.UsuarioService;
 
 
-@Controller
+@RestController
 @RequestMapping( value = "/api/usuarios")
 public class UsuarioController {
 
